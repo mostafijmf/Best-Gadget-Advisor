7fc8541b7722ec788b30a483dfcbf2b0fbe91971
Mostafij Mozumdar <91069030+mostafijmf@users.noreply.github.com>
2023-09-26 21:16:46 +0600

    Update dbConnect.js

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // database connection
 export async function dbConnect() {
-    await mongoose.connect(process.env.NEXT_PUBLIC_DB_LOCAL_URL)
+    await mongoose.connect(process.env.NEXT_PUBLIC_DB_URL)
         .then(() => {
             console.log('Database is connected.');
         })
